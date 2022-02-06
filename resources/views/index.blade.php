39e8cfed7b10b3cbd2c3ee0fb730f598f0842f54
Dario Mercuri <dario.mercuri31@gmail.com>
2022-02-05 20:09:19 -0500

    small homepage fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,18 +2,36 @@
 
 @section('content')
 <div class="">
+    <h2 class="text-4xl font-bold py-10 text-center">
+        Welcome to my blog
+    </h2>
     <div class="w-4/5 m-auto">
         <p class="text-center text-2xl py-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales diam eu lacus imperdiet, in rutrum massa fringilla. Nam rhoncus condimentum nunc non vehicula. Cras ac enim ipsum. Pellentesque fringilla iaculis leo, vestibulum fringilla augue maximus ac. Quisque vel nibh tempor, malesuada turpis eu, finibus orci. Phasellus at eleifend magna. Morbi eu enim sem. Aenean rutrum, tellus in cursus varius, ligula orci dictum risus, et lacinia dolor enim sed velit. Sed aliquam purus sed consectetur bibendum. Maecenas et sem dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eleifend odio condimentum mattis cursus. Integer non lobortis augue. Vestibulum orci mauris, suscipit vel risus eu, luctus faucibus tellus. Nunc ut suscipit augue, vitae vestibulum velit. Etiam sit amet euismod lorem.</p>
     </div>
-    <h1 class="text-center text-5xl">Recent Post</h1>
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-500">
-        <div>
-            <img src="https://via.placeholder.com/150" alt="" class="object-fill">
-        </div>
-        <div>
-            <h2 class="text-2xl py-3">Post Title</h2>
-            <p class="text-1xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sodales diam eu lacus imperdiet, in rutrum massa fringilla. Nam rhoncus condimentum nunc non vehicula. Cras ac enim ipsum. Pellentesque fringilla iaculis leo, vestibulum fringilla augue maximus ac. Quisque vel nibh tempor, malesuada turpis eu, finibus orci. Phasellus at eleifend magna. Morbi eu enim sem. Aenean rutrum, tellus in cursus varius, ligula orci dictum risus, et lacinia dolor enim sed velit. Sed aliquam purus sed consectetur bibendum. Maecenas et sem dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
-        </div>
+    <hr/>
+    <div class="text-center py-10">
+        <h2 class="text-4xl font-bold py-5">
+            Recent Posts
+        </h2>
     </div>
+
+    <div class="sm:grid grid-cols-2 w-4/5 m-auto">
+        <div class="flex bg-yellow-700 text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+                <span class="uppercase text-xs">
+                    Title
+                </span>
+
+                <h3 class="text-xl font-bold py-10">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
+                </h3>
+
+                <a 
+                    href=""
+                    class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                    Read More
+                </a>
+            </div>
+        </div>
 </div>
 @endsection
